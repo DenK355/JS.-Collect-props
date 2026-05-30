014894e8b0cd2a755ece244178a6d76e257d1167
Denis <bezsaluy@gmail.com>
2026-05-30 15:51:51 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 'use strict'
+class Person {
+    constructor(firstName, lastName, nickName, email){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.email = email;
+
+    }
+}
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const firstName = document.getElementById('name').value;
+    const lastName = document.getElementById('last-name').value;
+    const nickName = document.getElementById('nick-name').value;
+    const email = document.getElementById('email').value;
+
+    const person = new Person(firstName, lastName, nickName, email);
+
+    console.log(person)
+
+    localStorage.setItem('person', JSON.stringify(person));
+})
+
+
